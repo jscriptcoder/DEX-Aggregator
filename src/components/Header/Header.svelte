10ca46cd1113acfef773ccc26dea2a5227f4c2f0
Francisco <jscriptcoder@gmail.com>
2023-09-20 15:55:58 +0200

    add logo

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -4,7 +4,13 @@
 </script>
 
 <header>
-  <h1>DEX Aggregator</h1>
+  <div class="flex items-center space-x-2">
+    <div class="mask mask-squircle">
+      <img src="/dex-logo.png" alt="logo" />
+    </div>
+    <h1>DEX Aggregator</h1>
+  </div>
+
   <div class="flex items-center space-x-4">
     <ConnectButton />
     <ThemeButton />
@@ -14,5 +20,9 @@
 <style lang="postcss">
   header {
     @apply flex justify-between items-center py-4;
+  }
+
+  h1 {
+    @apply text-lg font-medium;
   }
 </style>
